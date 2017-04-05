49527948092b00c107fd467398c4dfd3a7330b55
webzuu <info@webzuu.pl>
2017-04-05 10:19:10 +0200

    adding platform_path helper

--- src/Dotenv.php
+++ src/Dotenv.php
@@ -8,10 +8,24 @@ class Dotenv
   protected $directory;
   protected $file;
 
+  public static $map = [];
+
   public function __construct($directory, $file = '.env')
   {
     $this->directory = $directory;
     $this->file = $file;
+  }
+
+  public static function env($id)
+  {
+    return getenv($id);
+  }
+
+  public static function platform_path($key)
+  {
+    $envPath = static::env($key);
+    $origin = static::$map[$key];
+    return $origin . '/' . $envPath;
   }
 
   public function load()
@@ -23,9 +37,18 @@ class Dotenv
       $path = DIRECTORY_SEPARATOR . implode(DIRECTORY_SEPARATOR, array_slice($dirs, 0, $i));
       $candidatePath = $path . DIRECTORY_SEPARATOR . $this->file;
       if (file_exists($candidatePath)) {
+        $this->map($candidatePath);
         $dotenv = new OriginalDotenv($path, $this->file);
         $dotenv->overload();
       }
+    }
+  }
+
+  protected function map($file)
+  {
+    $variables = parse_ini_file($file);
+    foreach($variables as $key => $value) {
+      static::$map[$key] = pathinfo($file, PATHINFO_DIRNAME);
     }
   }
 }
--- src/helpers.php
+++ src/helpers.php
@@ -1,9 +1,19 @@
 <?php
 
+use Zuu\Dotenv\Dotenv;
+
 if (!function_exists('env')) {
 
   function env($id) {
-    return getenv($id);
+    return Dotenv::env($id);
+  }
+
+}
+
+if (!function_exists('platform_path')) {
+
+  function platform_path($key) {
+    return Dotenv::platform_path($key);
   }
 
 }
